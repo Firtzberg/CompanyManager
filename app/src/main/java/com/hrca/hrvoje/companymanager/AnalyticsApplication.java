475493e0a12cbe1ccaa99d7adaa3cb638b7d770d
Firtzberg <hrvoje_jukic@hotmail.com>
2018-03-30 13:39:34 +0200

    Added Analytics support

---
+++ app/src/main/java/com/hrca/hrvoje/companymanager/AnalyticsApplication.java
@@ -0,0 +1,26 @@
+package com.hrca.hrvoje.companymanager;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+/**
+ * Created by hrvoje on 05.03.16..
+ */
+public class AnalyticsApplication extends Application {
+    private Tracker mTracker;
+
+    /**
+     * Gets the default {@link Tracker} for this activity.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
+    }
+}
